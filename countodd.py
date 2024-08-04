96304290cacad81363f739db1ef1c952a450c0df
Rahul Krishna K R <rahulkridhna@gmail.com>
2024-08-04 18:33:31 +0530

    Create countodd.py

---
+++ countodd.py
@@ -0,0 +1,15 @@
+'''
+  Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+ 
+'''
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        if low%2==0:
+            low+=1
+        if high%2==0:
+            high-=1
+        if low>high:
+            return 0
+        return (high-low)//2 + 1
+        
