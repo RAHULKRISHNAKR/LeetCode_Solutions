fa1aa06ef1aa2cfa61592259de851363031fad50
RAHULKRISHNAKR <118062510+RAHULKRISHNAKR@users.noreply.github.com>
2025-06-21 20:09:38 +0530

    huhu

---
+++ 3085MinimumDeletions.py
@@ -0,0 +1,25 @@
+'''
+You are given a string word and an integer k.
+
+We consider word to be k-special if |freq(word[i]) - freq(word[j])| <= k for all indices i and j in the string.
+
+Here, freq(x) denotes the frequency of the character x in word, and |y| denotes the absolute value of y.
+
+Return the minimum number of characters you need to delete to make word k-special.
+
+'''
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        cnt = defaultdict(int)
+        for c in word:
+            cnt[c] += 1
+        res = len(word)
+        for a in cnt.values():
+            deleted = 0
+            for b in cnt.values():
+                if a > b:
+                    deleted += b
+                elif b > a + k:
+                    deleted += b - (a + k)
+            res = min(res, deleted)
+        return res
